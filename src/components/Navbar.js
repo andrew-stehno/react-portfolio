8f229de3153c8c3e9c1ffea4ba23760848790c24
andrew stehno <andrew_stehno@yahoo.com>
2020-07-24 11:10:30 -0600

    created folder structure and set up navbar.js + css module

---
+++ src/components/Navbar.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import classes from './Navbar.module.css';
+
+const navbar = () => {
+    return (
+        <h1 className={classes.NavigationText}>This will be my navigation menu!</h1>
+    );
+};
+
+export default navbar;
