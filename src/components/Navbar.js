d0d8b64a17aff51123f7017d8b8b2659ef943b05
andrew stehno <andrew_stehno@yahoo.com>
2020-07-24 11:55:24 -0600

    Creating basic navbar layout

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,7 +3,14 @@ import classes from './Navbar.module.css';
 
 const navbar = () => {
     return (
-        <h1 className={classes.NavigationText}>This will be my navigation menu!</h1>
+        <div className={classes.NavBorder}>
+            <h1 className={classes.NavigationText}>Andrew Stehno</h1>
+            <nav className={classes.NavLinks}>
+                <a href="#top">About Me</a> | 
+                <a href="#top">Portfolio</a> | 
+                <a href="#top">Contact</a> 
+            </nav>
+        </div>
     );
 };
 
