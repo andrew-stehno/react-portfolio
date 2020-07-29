069541fe357ba8ed2be721f0847435d83ad56e24
andrew stehno <andrew_stehno@yahoo.com>
2020-07-29 13:43:24 -0600

    experimenting with media queries

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,17 +1,19 @@
-import React from 'react';
-import classes from './Navbar.module.css';
+import React from "react";
+import classes from "./Navbar.module.css";
 
 const navbar = () => {
-    return (
-        <div className={classes.NavBorder}>
-            <h1 className={classes.NavigationText}>Andrew Stehno</h1>
-            <nav className={classes.NavLinks}>
-                <a href="/">About Me</a> | 
-                <a href="/portfolio">Portfolio</a> | 
-                <a href="/contact">Contact</a> 
-            </nav>
-        </div>
-    );
+  return (
+    <div className={classes.NavBorder}>
+      <h1 className={classes.NavigationText}>Andrew Stehno</h1>
+      <nav className={classes.NavLinks}>
+        <a href="/">About</a> |<a href="/portfolio">Portfolio</a> |
+        <a href="/contact">Contact</a> |
+        <a href="https://docs.google.com/document/d/1FgA8RNcyV-B5uYWL0tm5fRTi93I_Qc7WCawcyMGZut0/edit?usp=sharing">
+          Resume
+        </a>
+      </nav>
+    </div>
+  );
 };
 
 export default navbar;
