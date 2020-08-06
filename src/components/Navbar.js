c66cc802489394df5f1272d1262b549d5ef08783
andrew stehno <andrew_stehno@yahoo.com>
2020-08-06 11:35:48 -0600

    added home route, changes to navbar css

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,15 +3,17 @@ import classes from "./Navbar.module.css";
 
 const navbar = () => {
   return (
-    <div className={classes.NavBorder}>
-      <h1 className={classes.NavigationText}>Andrew Stehno</h1>
-      <nav className={classes.NavLinks}>
-        <a href="/">About</a> |<a href="/portfolio">Portfolio</a> |
-        <a href="/contact">Contact</a> |
-        <a href="https://docs.google.com/document/d/1FgA8RNcyV-B5uYWL0tm5fRTi93I_Qc7WCawcyMGZut0/edit?usp=sharing">
-          Resume
-        </a>
-      </nav>
+    <div>
+        <h1 className={classes.NavigationText}>Andrew Stehno</h1>
+        <nav className={classes.NavLinks}>
+          <a href="/">Home</a> |
+          <a href="/about">About</a> |
+          <a href="/portfolio">Portfolio</a> |
+          <a href="/contact">Contact</a> |
+          <a href="https://docs.google.com/document/d/1FgA8RNcyV-B5uYWL0tm5fRTi93I_Qc7WCawcyMGZut0/edit?usp=sharing">
+            Resume
+          </a>
+        </nav>
     </div>
   );
 };
