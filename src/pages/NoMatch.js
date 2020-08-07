bb56913bf3f378a4377873b28f697387c15affc5
andrew stehno <andrew_stehno@yahoo.com>
2020-08-07 10:40:04 -0600

    created Error component with css sheet

--- src/pages/NoMatch.js
+++ src/pages/NoMatch.js
@@ -1,14 +1,10 @@
 import React from "react";
+import Error from "../components/Error";
 
 const NoMatch = () => {
   return (
     <div>
-      <h1>404 Page Not Found</h1>
-      <h1>
-        <span role="img" aria-label="Face With Rolling Eyes Emoji">
-          🙄
-        </span>
-      </h1>
+      <Error/>
     </div>
   );
 };
